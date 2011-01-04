be1fc8a641cb70c81f861f56fd24c2beb22422c6
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-01-04 20:44:15 +0000

    - Changed sess_time_to_update to 5 minutes

--- application/config/config.php
+++ application/config/config.php
@@ -247,7 +247,7 @@ $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'clearos_webconfig';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 10;
+$config['sess_time_to_update']	= 300;
 
 /*
 |--------------------------------------------------------------------------
