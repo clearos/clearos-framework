bd719f7f284518c35719e709b78694ff596a38f8
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-03-12 03:13:41 +0000

    - Added more menu translations

--- application/language/en_US/menu_lang.php
+++ application/language/en_US/menu_lang.php
@@ -1,11 +1,10 @@
 <?php
 
 $lang['menu_category_system'] = 'System';
-$lang['menu_category_directory'] = 'Directory';
 $lang['menu_category_network'] = 'Network';
-$lang['menu_category_server'] = 'Server';
 $lang['menu_category_gateway'] = 'Gateway';
-$lang['menu_category_reports'] = 'Reports';
+$lang['menu_category_server'] = 'Server';
+$lang['menu_category_applications'] = 'Applications';
 
 $lang['menu_subcategory_infrastructure'] = 'Infrastructure';
 $lang['menu_subcategory_settings'] = 'Settings';
@@ -15,4 +14,7 @@ $lang['menu_subcategory_file_and_print'] = 'File and Print';
 $lang['menu_subcategory_intrusion_protection'] = 'Intrusion Protection';
 $lang['menu_subcategory_antimalware'] = 'Antimalware';
 $lang['menu_subcategory_mail'] = 'Mail'; 
+$lang['menu_subcategory_mail_scanning'] = 'Mail Scanning'; 
 $lang['menu_subcategory_database'] = 'Database';
+$lang['menu_subcategory_accounts'] = 'Accounts';
+$lang['menu_subcategory_directory'] = 'Directory';
