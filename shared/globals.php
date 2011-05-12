f8297e198283053b87e5626b4468de2070406223
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-05-12 01:36:15 +0000

    - Changed FIXME

--- shared/globals.php
+++ shared/globals.php
@@ -99,7 +99,7 @@ if (Config::$debug_mode) {
 
 function clearos_app_htdocs($app = NULL)
 {
-    // FIXME: fix inconsistent naming
+    // FIXME: uri_string is CodeIgniter-ism and not always loaded
     if (is_null($app))
         $app = uri_string();
 
