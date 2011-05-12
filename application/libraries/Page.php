d00f4c838b38a4df623b8b040fb5cc075c0b6b90
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-05-12 21:49:57 +0000

    - Added PHP extension to global javascript file

--- application/libraries/Page.php
+++ application/libraries/Page.php
@@ -544,7 +544,7 @@ class MY_Page
 
 <!-- Jquery -->
 <script type='text/javascript' src='/js/jquery-1.4.4.min.js'></script>
-<script type='text/javascript' src='/js/clearos-6.0.0.js'></script>
+<script type='text/javascript' src='/js/clearos-6.0.0.js.php'></script>
 ";
 
         // <head> extras defined in theme (head.php)
--- htdocs/js/clearos-6.0.0.js.php
+++ htdocs/js/clearos-6.0.0.js.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Daemon ajax helper.
+ * Global ajax helpers.
  *
  * @category   ClearOS
  * @package    Base
@@ -9,7 +9,7 @@
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
- * @link       http://www.clearfoundation.com/docs/developer/apps/base/
+ * @link       http://www.clearfoundation.com/docs/developer/framework/base/
  */
 
 ///////////////////////////////////////////////////////////////////////////////
