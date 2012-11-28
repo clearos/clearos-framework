94f3621a367fac113339c1bf0911e595a06ed779
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-28 19:06:52 +0000

    - Added 'text/plain' to csv entry

--- application/config/mimes.php
+++ application/config/mimes.php
@@ -8,9 +8,10 @@
 |
 */
 
+// ClearFoundation - added 'text/plain' to csv entry
 $mimes = array(	'hqx'	=>	'application/mac-binhex40',
 				'cpt'	=>	'application/mac-compactpro',
-				'csv'	=>	array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel'),
+				'csv'	=>	array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain'),
 				'bin'	=>	'application/macbinary',
 				'dms'	=>	'application/octet-stream',
 				'lha'	=>	'application/octet-stream',
