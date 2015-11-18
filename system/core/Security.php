b309accc69eca5846f8cdb5d708c3332c543d84b
Benjamin Chambers <bchambers@clearfoundation.com>
2015-11-18 12:19:13 -0500

    Patched regex causing post data with ampersand to be appended with ; [Tracker #6271]

--- system/core/Security.php
+++ system/core/Security.php
@@ -820,7 +820,7 @@ class CI_Security {
 		 * the conversion of entities to ASCII later.
 		 *
 		 */
-		$str = preg_replace('#(&\#?[0-9a-z]{2,})([\x00-\x20])*;?#i', "\\1;\\2", $str);
+		$str = preg_replace('#(&\#?[0-9a-z]{2,})([\x00-\x20])+;?#i', "\\1;\\2", $str);
 
 		/*
 		 * Validate UTF16 two byte encoding (x00)
