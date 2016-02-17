0c111c5605ba8680d3d0eddad5fd660a19fa10e1
pcbaldwin <pbaldwin@clearfoundation.com>
2016-02-17 04:28:53 -0500

    Backport error reporting changes [tracker #6091]

--- shared/globals.php
+++ shared/globals.php
@@ -10,7 +10,7 @@
  * @package    Shared
  * @subpackage Helpers
  * @author     ClearFoundation <developer@clearfoundation.com>
- * @copyright  2011 ClearFoundation
+ * @copyright  2011-2016 ClearFoundation
  * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
  * @link       http://www.clearfoundation.com/docs/developer/framework/
  */
@@ -443,13 +443,11 @@ function clearos_theme_url($theme)
 
 function _clearos_error_handler($errno, $errmsg, $file, $line, $context)
 {
-    // If the @ symbol was used to suppress errors, bail
-    //--------------------------------------------------
+    // If developer requests error suppression, then do so
+    //----------------------------------------------------
 
-    /* FIXME -- revisit this ... it seems to suppress too much.
     if (error_reporting(0) === 0)
         return;
-    */
 
     // Log the error
     //--------------
