8d6d9026cccb4e45f18b93d3e9c2c91a737eb573
pcbaldwin <peter@egloo.ca>
2017-06-12 23:22:39 -0400

    Reverted CSRF token name

--- application/config/config.php
+++ application/config/config.php
@@ -457,8 +457,8 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
 $config['csrf_protection'] = TRUE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_token_name'] = 'ci_csrf_token';
+$config['csrf_cookie_name'] = 'ci_csrf_token';
 $config['csrf_expire'] = 0;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
