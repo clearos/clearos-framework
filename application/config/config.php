f8bae57a0e95a095dccf6baa924d13f837186093
pcbaldwin <peter@egloo.ca>
2017-06-19 11:57:21 -0400

    Added support for non-browser access

--- application/config/config.php
+++ application/config/config.php
@@ -16,8 +16,10 @@
 */
 // ClearFoundation - configuration is custom of course
 // ClearFoundation - automatically detect base URL
+$http_host = (empty($_SERVER['HTTP_HOST'])) ? '127.0.0.1' : $_SERVER['HTTP_HOST'];
+
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= '://' . $_SERVER['HTTP_HOST'] . '/app/';
+$config['base_url'] .= '://' . $http_host . '/app/';
 
 /*
 |--------------------------------------------------------------------------
